ae13d79b77a7f96a132e63c4fb43c3758c4cb189
KG <davidgjy@163.com>
2017-04-25 23:33:53 +0800

    multi thread test

--- zeus-test/src/main/java/zeus/test/infra/random/getRandomStringTest.java
+++ zeus-test/src/main/java/zeus/test/infra/random/getRandomStringTest.java
@@ -6,7 +6,27 @@ import zeus.infra.rand.IdGenerator;
  * Created by KG on 2017/4/25.
  */
 public class getRandomStringTest {
+    private static void singleThreadRandString() {
+        String randString = IdGenerator.getRandomString(16);
+        System.out.println("Random String: " + randString);
+    }
+
+    private static void multiThreadRandString() {
+        for (int i = 0; i < 20; i++) {
+            Runnable myRunnable = new RandomStringRunnable();
+            Thread t = new Thread(myRunnable);
+            t.start();
+        }
+    }
+
     public static void main(String args[]) {
+        //singleThreadRandString();
+        multiThreadRandString();
+    }
+}
+
+class RandomStringRunnable implements Runnable {
+    public void run() {
         String randString = IdGenerator.getRandomString(16);
         System.out.println("Random String: " + randString);
     }
