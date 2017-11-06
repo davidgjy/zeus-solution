c9e2b2d05e8838535f0a19db1c497963911cf2f0
KG <gujy@dadi18.com>
2017-11-06 15:35:14 +0800

    Client Server Demo with old Java and Java 8

---
+++ zeus-network/src/main/java/zeus/network/start/SimpleEchoServerJ8.java
@@ -0,0 +1,52 @@
+package zeus.network.start;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.function.Supplier;
+import java.util.stream.Stream;
+
+public class SimpleEchoServerJ8 {
+
+    public static void main(String[] args) {
+        System.out.println("Simple Echo Server");
+        try (ServerSocket serverSocket = new ServerSocket(6000)){
+            System.out.println("Waiting for connection.....");
+
+            Socket clientSocket = serverSocket.accept();
+            System.out.println("Connected to client");
+
+            try (BufferedReader br = new BufferedReader(
+                    new InputStreamReader(clientSocket.getInputStream()));
+                 PrintWriter out = new PrintWriter(
+                         clientSocket.getOutputStream(), true)) {
+
+                // Functional implementation
+                Supplier<String> socketInput = () -> {
+                    try {
+                        return br.readLine();
+                    } catch (IOException ex) {
+                        return null;
+                    }
+                };
+                Stream<String> stream = Stream.generate(socketInput);
+                stream
+                        .map(s -> {
+                            System.out.println("Client request: " + s);
+                            out.println(s);
+                            return s;
+                        })
+                        .allMatch(s -> s != null);
+            } catch (IOException ex) {
+                ex.printStackTrace();
+            }
+
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        }
+        System.out.println("Simple Echo Server Terminating");
+    }
+}
