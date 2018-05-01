f7a9f3b925a19f156ed4261956ebc48f0329b778
KG <davidgjy@163.com>
2018-05-01 13:49:48 +0800

    thread demo

---
+++ zeus-playground/src/main/java/zeus/playground/thread/RunnableDemo.java
@@ -0,0 +1,39 @@
+package zeus.playground.thread;
+
+/**
+ * @author: KG
+ * @description:
+ * @date: Created in 下午1:41 2018/5/1
+ * @modified by:
+ */
+
+/**
+ * 通过实现Runnable接口来创建线程
+  */
+public class RunnableDemo implements Runnable {
+    private int i;
+
+    // run方法同样是线程执行体
+    @Override
+    public void run() {
+        for (; i < 100; i++) {
+            // 当线程类实现Runnable接口时，
+            // 如果想获取当前线程，只能用Thread.currentThread()方法。
+            System.out.println(Thread.currentThread().getName()
+                    + "  " + i);
+        }
+    }
+
+    public static void main(String[] args) {
+        for (int i = 0; i < 100; i++) {
+            System.out.println(Thread.currentThread().getName() + "  " + i);
+            if (i == 20) {
+                RunnableDemo st = new RunnableDemo();
+                // 通过new Thread(target , name)方法创建新线程
+                new Thread(st, "新线程1").start();
+                new Thread(st, "新线程2").start();
+            }
+        }
+    }
+}
+
---
+++ zeus-playground/src/main/java/zeus/playground/thread/ThreadDemo.java
@@ -0,0 +1,39 @@
+package zeus.playground.thread;
+
+/**
+ * @author: KG
+ * @description:
+ * @date: Created in 下午1:34 2018/5/1
+ * @modified by:
+ */
+
+/**
+ * 通过继承Thread类来创建线程类
+ */
+public class ThreadDemo extends Thread {
+    private int i;
+
+    @Override
+    public void run() {
+        for (; i < 100; i++) {
+            // 当线程类继承Thread类时，直接使用this即可获取当前线程
+            // Thread对象的getName()返回当前该线程的名字
+            // 因此可以直接调用getName()方法返回当前线程的名
+            System.out.println(getName() + " " + i);
+        }
+    }
+
+    public static void main(String[] args) {
+        for (int i = 0; i < 100; i++) {
+            // 调用Thread的currentThread方法获取当前线程
+            System.out.println(Thread.currentThread().getName() + " " + i);
+            if (i == 20) {
+                // 创建、并启动第一条线程
+                new ThreadDemo().start();
+                // 创建、并启动第二条线程
+                new ThreadDemo().start();
+            }
+        }
+    }
+}
+
