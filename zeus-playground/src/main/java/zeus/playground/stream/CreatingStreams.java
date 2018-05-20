e6b2cbb4d74575cb14b2002423e6581b9b15abe4
Kelvin Gu <davidgjy518@gmail.com>
2018-05-20 21:00:30 +0800

    adjustment

--- zeus-playground/src/main/java/zeus/playground/stream/CreatingStreams.java
+++ zeus-playground/src/main/java/zeus/playground/stream/CreatingStreams.java
@@ -13,6 +13,7 @@ import java.util.stream.Stream;
 
 public class CreatingStreams {
     public static <T> void show(String title, Stream<T> stream) {
+
         final int SIZE = 10;
         List<T> firstElements = stream
                 .limit(SIZE + 1)
