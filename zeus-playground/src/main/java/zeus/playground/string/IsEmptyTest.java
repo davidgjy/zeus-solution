c6983f157b92c0eb5cb18447b2b629aa353f93e0
Kelvin Gu <davidgjy518@gmail.com>
2018-07-11 10:11:00 +0800

    string test

---
+++ zeus-playground/src/main/java/zeus/playground/string/IsEmptyTest.java
@@ -0,0 +1,16 @@
+package zeus.playground.string;
+
+/**
+ * @author KG(Kelvin Gu)
+ * @ClassName: IsEmptyTest
+ * @Package zeus.playground.string
+ * @Description: test
+ * @date 2018/7/3 17:21
+ */
+public class IsEmptyTest {
+    public static void main(String[] args) {
+        String a = "  ";
+        //System.out.println(a!=null && a.equals(""));
+        System.out.println(a.equals(""));
+    }
+}
