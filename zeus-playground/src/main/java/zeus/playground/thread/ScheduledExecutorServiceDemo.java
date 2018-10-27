edd8857f204cde884e2d02ebe4544d7de970df73
KG <davidgjy@163.com>
2018-10-27 13:31:11 +0800

    scheduled executor service

---
+++ zeus-playground/src/main/java/zeus/playground/thread/ScheduledExecutorServiceDemo.java
@@ -0,0 +1,31 @@
+package zeus.playground.thread;
+
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
+
+/**
+ * @author: KG
+ * @description:
+ * @date: Created in 上午11:45 2018/10/27
+ * @modified by:
+ */
+
+public class ScheduledExecutorServiceDemo {
+
+    public static void main(String[] args) {
+        int threadSize = 10;
+        ScheduledExecutorService ses = Executors.newScheduledThreadPool(threadSize);
+        ses.scheduleAtFixedRate(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    Thread.sleep(1000);
+                    System.out.println(System.currentTimeMillis() / 1000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        }, 0, 2, TimeUnit.SECONDS);
+    }
+}
