04fb55781ee69e85c8ffe0f040527b14541b0f5a
KG <davidgjy@163.com>
2020-07-17 20:41:09 +0800

    zeus infra

--- zeus-infra/src/main/java/zeus/infra/performance/TimeConsole.java
+++ zeus-infra/src/main/java/zeus/infra/performance/TimeConsole.java
@@ -5,6 +5,7 @@ package zeus.infra.performance;
  */
 
 public class TimeConsole implements TimeDevice {
+    @Override
     public void print(String moduleName, String time, long seconds) {
         System.out.println("******************************* Performance Log *******************************");
         System.out.println("Module: " + moduleName + ", Time Consumed: " + time + ", 性能: " + WarnMsg.infoBySeconds(seconds));
--- zeus-infra/src/main/java/zeus/infra/performance/TimeLog.java
+++ zeus-infra/src/main/java/zeus/infra/performance/TimeLog.java
@@ -1,18 +1,18 @@
 package zeus.infra.performance;
 
+import lombok.extern.slf4j.Slf4j;
 import org.apache.log4j.Logger;
 
 /**
  * Created by KG on 2017/7/22.
  */
-
+@Slf4j
 public class TimeLog implements TimeDevice {
-    Logger logger = Logger.getLogger(TimeLog.class);
-
+    @Override
     public void print(String moduleName, String time, long seconds) {
-        logger.info("******************************* Performance Log *******************************");
-        logger.info("Module: " + moduleName + ", Time Consumed: " + time + ", 性能: " + WarnMsg.infoBySeconds(seconds));
-        logger.info("*******************************************************************************");
+        log.info("******************************* Performance Log *******************************");
+        log.info("Module: " + moduleName + ", Time Consumed: " + time + ", 性能: " + WarnMsg.infoBySeconds(seconds));
+        log.info("*******************************************************************************");
     }
 }
 
