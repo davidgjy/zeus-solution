a72d47e6fdf84805dadb1b28032e1603c4401dbe
KG <davidgjy@163.com>
2020-07-22 15:52:45 +0800

    BlockingQueue Demo

---
+++ zeus-playground/src/main/java/zeus/playground/concurrent/ConcurrentTest.java
@@ -0,0 +1,11 @@
+package zeus.playground.concurrent;
+
+/**
+ * @author: KG
+ * @description:
+ * @date: Created in 3:23 下午 2020/7/22
+ * @modified by:
+ */
+
+public class ConcurrentTest {
+}
---
+++ zeus-playground/src/main/java/zeus/playground/concurrent/queue/SearchFileByBlockingQueueDemo.java
@@ -0,0 +1,104 @@
+package zeus.playground.concurrent.queue;
+
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.Scanner;
+import java.util.concurrent.ArrayBlockingQueue;
+import java.util.concurrent.BlockingQueue;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+/**
+ * @author: KG
+ * @description:
+ * @date: Created in 3:24 下午 2020/7/22
+ * @modified by:
+ */
+
+public class SearchFileByBlockingQueueDemo {
+    private static final int FILE_QUEUE_SIZE = 10;
+    private static final int SEARCH_THREADS = 100;
+    private static final Path DUMMY = Path.of("");
+    private static BlockingQueue<Path> queue = new ArrayBlockingQueue<>(FILE_QUEUE_SIZE);
+
+    public static void main(String[] args) {
+        try (var in = new Scanner(System.in)) {
+            System.out.print("Enter base directory (e.g. /opt/jdk-9-src): ");
+            String directory = in.nextLine();
+            System.out.print("Enter keyword (e.g. volatile): ");
+            String keyword = in.nextLine();
+
+            Runnable enumerator = () -> {
+                try {
+                    enumerate(Path.of(directory));
+                    queue.put(DUMMY);
+                } catch (IOException e) {
+                    e.printStackTrace();
+                } catch (InterruptedException e) {
+                }
+            };
+
+            // 工作线程负责向队列添加元素
+            new Thread(enumerator).start();
+
+            for (int i = 1; i <= SEARCH_THREADS; i++) {
+                Runnable searcher = () -> {
+                    try {
+                        var done = false;
+                        while (!done) {
+                            Path file = queue.take();
+                            if (file == DUMMY) {
+                                queue.put(file);
+                                done = true;
+                            } else {
+                                search(file, keyword);
+                            }
+                        }
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    } catch (InterruptedException e) {
+                    }
+                };
+                // 工作线程负责从队列移除元素
+                new Thread(searcher).start();
+            }
+        }
+    }
+
+    /**
+     * Recursively enumerates all files in a given directory and its subdirectories.
+     * @param directory the directory in which to start
+     */
+    public static void enumerate(Path directory) throws IOException, InterruptedException {
+        try (Stream<Path> children = Files.list(directory)) {
+            for (Path child : children.collect(Collectors.toList())) {
+                if (Files.isDirectory(child)) {
+                    enumerate(child);
+                } else {
+                    queue.put(child);
+                }
+            }
+        }
+    }
+
+    /**
+     * Searches a file for a given keyword and prints all matching lines.
+     *
+     * @param file    the file to search
+     * @param keyword the keyword to search for
+     */
+    public static void search(Path file, String keyword) throws IOException {
+        try (var in = new Scanner(file, StandardCharsets.UTF_8)) {
+            int lineNumber = 0;
+            while (in.hasNextLine()) {
+                lineNumber++;
+                String line = in.nextLine();
+                if (line.contains(keyword)) {
+                    System.out.printf("%s:%d:%s%n", file, lineNumber, line);
+                }
+            }
+        }
+    }
+}
